8b46823c12e01a55179b90fccc5960a5922ebbfd
Rahul bishnoi <rahulbhadu14@gmail.com>
2023-04-19 18:03:18 +0530

    speech to text version 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
 
-function App() {
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import useClipboard from "react-use-clipboard";
+
+
+const App = () => {
+
+  const [lang, setlang] = useState("en-IN");
+
+  const handleLanguage = (event) => {
+    setlang(event.target.value);
+  }
+
+  const startlisten = () => { SpeechRecognition.startListening({ continuous: true, language: lang }) }
+  const stoplisten = () => { SpeechRecognition.stopListening(); }
+
+
+
+  const { transcript, listening, resetTranscript, browserSupportsSpeechRecognition } = useSpeechRecognition()
+
+  const [isCopied, setCopied] = useClipboard(transcript);
+
+  if (!browserSupportsSpeechRecognition) {
+    return alert("browser not suppoting the app")
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='container'>
+        <h1>
+          S P E E C H  -TO-  T E X T
+        </h1>
+        <h4>Speak to convert</h4>
+
+        <div className='main-content'>
+          <p>{transcript?transcript:"speak..."}</p>
+        </div>
+
+        <div className='icons'>
+          <p><i>Microphone:</i> {listening ? 'on' : 'off'}</p>
+        <p><i>Language:</i> {lang}</p>
+
+        <p><i>Copied:</i> {isCopied ? "Yes!" : "Nope!"}</p>
+
+        </div>
+        
+        <div className='btn-box'>
+
+          <button onClick={setCopied}>
+            copy
+          </button>
+
+          <button onClick={startlisten}>start listening</button>
+          <button onClick={stoplisten}>stop listening</button>
+          <button onClick={resetTranscript}>clear</button>
+
+          <select value={lang} onChange={handleLanguage}>
+            <option value={"hi-IN"}>English</option>
+            <option value={"hi-IN"}>HINDI</option>
+            <option value={"fr-FR"}>French</option>
+            <option value={"es-ES"}>Spanish</option>
+            <option value={"ru-RU"}>Russian</option>
+
+          </select>
+
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
